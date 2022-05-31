8ca1e246070392afc1bddb0286c0d316fd635436
sweisss <sweisss@gmail.com>
2022-05-30 17:54:06 -0700

    First commit. File created

---
+++ Junkins_Weiss_FinalProject_SQLSchema.sql
@@ -0,0 +1,11 @@
+--CS340 Databases
+--Final Project - SQL Schema
+--Seth Weiss, Orion Junkins
+--weissse@oregonstate.edu
+--orion's email
+
+--this file builds the schema for an avlanche forecasting network in SQLite
+
+--before running the file from the command line, enter the following command:
+--$ sqlite3 FILENAME.db < FILENAME.sql
+
