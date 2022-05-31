75b5fae5f5a827db9c7b1c68f6737ab05413ebca
sweisss <sweisss@gmail.com>
2022-05-30 18:06:07 -0700

    Added in a skeletal structure for what relations will need to be created.

--- Junkins_Weiss_FinalProject_SQLSchema.sql
+++ Junkins_Weiss_FinalProject_SQLSchema.sql
@@ -25,4 +25,50 @@
 
 --Clear the way for the Agency table.
 DROP TABLE IF EXISTS Agency;
+--Create the Agency table
+--
+
+--Clear the way for the Forecaster table.
+DROP TABLE IF EXISTS Forecaster;
+--Create the Forecaster table
+--
+
+--Clear the way for the Forecast table.
+DROP TABLE IF EXISTS Forecast;
+--Create the Forecast table
+--
+
+--Clear the way for the Problem table.
+DROP TABLE IF EXISTS Problem;
+--Create the Problem table
+--
+
+--Clear the way for the Elevation table.
+DROP TABLE IF EXISTS Elevation;
+--Create the Elevation table
+--
+
+--Clear the way for the Aspect table.
+DROP TABLE IF EXISTS Aspect;
+--Create the Aspect table
+--
+
+--Clear the way for the Observation table.
+DROP TABLE IF EXISTS Observation;
+--Create the Observation table
+--
+
+--Clear the way for the Observer table.
+DROP TABLE IF EXISTS Observer;
+--Create the Observer table
+--
+
+--Clear the way for the Zone table.
+DROP TABLE IF EXISTS Zone;
+--Create the Zone table
+--
+
+--Clear the way for the Contribution table.
+DROP TABLE IF EXISTS Contribution;
+--Create the Contribution table
 --
