40959f37c28672525f5d653969bc5e5b94646b70
sweisss <sweisss@gmail.com>
2022-06-04 10:19:17 -0700

    started another interesting query. added notes to make others more interesting.

--- interesting_queries.sql
+++ interesting_queries.sql
@@ -5,7 +5,8 @@
 --and use one or more concepts such as WHERE filtering, GROUP BY, ORDER BY,
 --HAVING, UNION, aggregation, and/or nested queries.
 
--- Select all at-treeline forcasts from Jan 1, 2022. 
+-- Select all at-treeline forcasts from Jan 1, 2022.
+-- !!!THIS MIGHT BE TOO SIMPLE!!!
 SELECT danger_at_treeline FROM Forecast WHERE issue_date is "1/1/2022" LIMIT 5;
 
 -- Find all eastern aspect forecasts from Colorado and Utah 
@@ -18,10 +19,14 @@ NATURAL JOIN Agency
 WHERE Aspect.aspect = "E" AND Agency.agency_id=1 OR Agency.agency_id=6
 LIMIT 5;
 
--- Select a complete forecast with it's associated problems
+-- Select a complete forecast with it's associated problems 
+-- !!!THIS MIGHT BE TOO SIMPLE (maybe put and order by or group by in it)
 SELECT * FROM Forecast NATURAL JOIN Problem LIMIT 5;
 
--- Find the first and last names from forecasters who contributed observations listed alphabetically by last name.
+-- Find the first and last names from forecasters who contributed observations listed alphabetically by last name. 
+--!!!THIS MIGHT BE TOO SIMPLE!!!
 SELECT fname, lname FROM Observer WHERE observer_type="Forecaster" ORDER BY lname LIMIT 5; 
 
---Find all forecasters who have not contributed an observation
+--Find the first and last name of forecasters who have not contributed an observation
+--This doesn't work yet
+SELECT fname, lname FROM Forecaster NATURAL JOIN Observer WHERE observer_type iS NULL;
