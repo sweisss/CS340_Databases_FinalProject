b5ca6387b758e14b995d4ae9d6e21342d5dea277
orion-junkins <orionjunkins@gmail.com>
2022-06-05 11:39:47 -0700

    Updated transactions copied to php

--- web_app.php
+++ web_app.php
@@ -84,18 +84,28 @@
                 build_table($sql, $db);
             } else if (isset($_POST['canned_query_1'])) {
                 $sql = "SELECT a.agency_name, p.problem_type, COUNT(p.problem_type) FROM Forecast as fr INNER JOIN Problem as p ON fr.fid = p.fid INNER JOIN Agency as a ON fr.issued_by = a.agency_id WHERE issued_by=1 GROUP BY p.problem_type ORDER BY COUNT (p.problem_type) DESC LIMIT 1;";
+                
+                
                 build_table($sql, $db);
             } else if (isset($_POST['canned_query_2'])) {
-                $sql = "SELECT DISTINCT * FROM Forecast NATURAL JOIN Problem NATURAL JOIN Aspect NATURAL JOIN Forecaster NATURAL JOIN Agency WHERE Aspect.aspect = "E" AND Agency.agency_id=1 OR Agency.agency_id=6 LIMIT 5;";
+                $sql = "SELECT DISTINCT p.pid ,fr.issue_date, ag.agency_name, fr.corresponds_to as Zone, p.problem_type, p.size, p.likelihood, a.aspect, e.elevation FROM Forecast as fr JOIN Problem as p ON fr.fid = p.fid JOIN Aspect as a  ON p.pid = a.pid JOIN Elevation as e  ON p.pid = e.pid JOIN Forecaster as f ON fr.issued_by = f.frcstr_id JOIN Agency as ag ON ag.agency_id = f.agency_id WHERE a.aspect = "E" AND ag.agency_id=6 LIMIT 5;";                 
+                
+                
                 build_table($sql, $db);
             } else if (isset($_POST['canned_query_3'])) {
-                $sql = "SELECT * FROM Forecast NATURAL JOIN Problem WHERE problem.problem_type = "Wind Slab" ORDER BY corresponds_to, issue_date LIMIT 5;";
+                $sql = "SELECT fid, issue_date, danger_below_treeline, danger_at_treeline, danger_above_treeline, issued_by as Agency, corresponds_to AS Zone, pid, problem_type, size, likelihood FROM Forecast  NATURAL JOIN Problem  WHERE problem.problem_type = "Wind Slab" ORDER BY corresponds_to, issue_date LIMIT 5;";
+                
+                
                 build_table($sql, $db);
             } else if (isset($_POST['canned_query_4'])) {
-                $sql = "SELECT DISTINCT o.observation_date, z.zone_name, o.observation_location, CASE WHEN avalanche = 0 THEN 'No' ELSE 'Yes' END AS avalanche, o.obs_description FROM Observation AS o NATURAL JOIN Zone AS z NATURAL JOIN Agency AS a WHERE agency_id IN (0, 3) LIMIT 5;";
+                $sql = "SELECT DISTINCT o.observation_date, z.zone_name, o.observation_location, CASE WHEN avalanche = 0 THEN 'No' ELSE 'Yes' END AS avalanche, o.obs_description  FROM Observation AS o NATURAL JOIN Zone AS z NATURAL JOIN Agency AS a  WHERE agency_id IN (0, 3) LIMIT 5;";
+                
+                
                 build_table($sql, $db);
             } else if (isset($_POST['canned_query_5'])) {
-                $sql = "SELECT f.fname, f.lname FROM Forecaster AS f LEFT JOIN Observer AS o ON f.fname = o.fname WHERE o.fname iS NULL;";
+                $sql = "SELECT f.fname, f.lname FROM Forecaster AS f LEFT JOIN Observer AS o ON f.fname = o.fname WHERE o.fname iS NULL; ";
+    
+
                 build_table($sql, $db);
             } else {
                 echo "<br>Please input a query</br>";
@@ -109,14 +119,16 @@
         <form method="post">
             <h4>Canned Query 1: Most common avalanche problem type for Utah</h4>
             <p> Identify the single most common type of problem from all forecasts issued by forecasters who work for the Utah Avalanche Center (agency_id=1). Return the agency name, the problem type, and the total count identified.</p> 
-            <p>SELECT a.agency_name, p.problem_type, COUNT(p.problem_type) <br>FROM Forecast as fr <br>INNER JOIN Problem as p <br>ON fr.fid = p.fid <br>INNER JOIN Agency as a <br>ON fr.issued_by = a.agency_id <br>WHERE issued_by=1 <br>GROUP BY p.problem_type <br>ORDER BY COUNT (p.problem_type) DESC <br>LIMIT 1; </p>
+            
+            <p>SELECT a.agency_name, p.problem_type, COUNT(p.problem_type) <br>FROM Forecast as fr <br>INNER JOIN Problem as p <br>ON fr.fid = p.fid <br>INNER JOIN Agency as a <br>ON fr.issued_by = a.agency_id <br>WHERE issued_by=1 <br>GROUP BY p.problem_type <br>ORDER BY COUNT (p.problem_type) DESC <br>LIMIT 1;</p>
             <input type='submit' name="canned_query_1" value="Submit Query 1"><br>
         </form>
         
         <!-- Query 2 -->
         <form method="post">
-            <h4>Canned Query 2:Find all forecasts from Colorado (agency_id=6) and Utah (agency_id = 1) where some problem exists on an eastern aspect. </h4>
-            <p>SELECT DISTINCT * <br> FROM Forecast <br> NATURAL JOIN Problem <br> NATURAL JOIN Aspect <br> NATURAL JOIN Forecaster <br> NATURAL JOIN Agency <br> WHERE Aspect.aspect = "E" AND Agency.agency_id=1 OR Agency.agency_id=6 <br> LIMIT 5; </p>
+            <h4>Canned Query 2: Search forecasts from Colorado (agency id of 6) which have a problem on an eastern aspect and return an overview of each problem.  </h4>
+            
+            <p>SELECT DISTINCT p.pid ,fr.issue_date, ag.agency_name, fr.corresponds_to as Zone, p.problem_type, p.size, p.likelihood, a.aspect, e.elevation <br>FROM Forecast as fr <br>JOIN Problem as p <br>ON fr.fid = p.fid <br>JOIN Aspect as a  <br>ON p.pid = a.pid <br>JOIN Elevation as e  <br>ON p.pid = e.pid <br>JOIN Forecaster as f <br>ON fr.issued_by = f.frcstr_id <br>JOIN Agency as ag <br>ON ag.agency_id = f.agency_id <br>WHERE a.aspect = "E" AND ag.agency_id=6 <br>LIMIT 5;</p>
             <input type='submit' name="canned_query_2" value="Submit Query 2"><br>
         </form>
         
@@ -124,21 +136,26 @@
         <!-- Query 3 -->
         <form method="post">
             <h4>Canned Query 3: Find all forecasts that involve a Wind Slab problem. Order first by region then by issue date.  </h4>
-            <p>SELECT * <br>FROM Forecast <br>NATURAL JOIN Problem <br>WHERE problem.problem_type = "Wind Slab"<br>ORDER BY corresponds_to, issue_date<br>LIMIT 5;</p>
+            
+            <p>SELECT fid, issue_date, danger_below_treeline, danger_at_treeline, danger_above_treeline, issued_by as Agency, corresponds_to AS Zone, pid, problem_type, size, likelihood <br>FROM Forecast  <br>NATURAL JOIN Problem  <br>WHERE problem.problem_type = "Wind Slab" <br>ORDER BY corresponds_to, issue_date <br>LIMIT 5;</p>
             <input type='submit' name="canned_query_3" value="Submit Query 3"><br>
         </form>
+
 
         <!-- Query 4 -->
         <form method="post">
             <h4>Canned Query 4: Find all observation data from Oregon.</h4>
-            <p>SELECT DISTINCT <br>    o.observation_date, <br>    z.zone_name, <br>    o.observation_location,<br>    CASE WHEN avalanche = 0 THEN 'No' ELSE 'Yes' END AS avalanche, <br>    o.obs_description <br>FROM Observation AS o<br>NATURAL JOIN Zone AS z<br>NATURAL JOIN Agency AS a <br>WHERE agency_id IN (0, 3)<br>LIMIT 5;</p>
+            
+            <p>SELECT DISTINCT  <br>    o.observation_date,  <br>    z.zone_name,  <br>    o.observation_location, <br>    CASE WHEN avalanche = 0 THEN 'No' ELSE 'Yes' END AS avalanche,  <br>    o.obs_description  <br>FROM Observation AS o <br>NATURAL JOIN Zone AS z <br>NATURAL JOIN Agency AS a  <br>WHERE agency_id IN (0, 3) <br>LIMIT 5;</p>
             <input type='submit' name="canned_query_4" value="Submit Query 4"><br>
         </form>
+
 
         <!-- Query 5 -->
         <form method="post">
             <h4>Canned Query 5: Find the first and last name of forecasters who have not contributed an observation.</h4>
-            <p>SELECT f.fname, f.lname <br>FROM Forecaster AS f <br>LEFT JOIN Observer AS o <br>ON f.fname = o.fname <br>WHERE o.fname iS NULL;</p>
+            
+            <p>SELECT f.fname, f.lname FROM Forecaster AS f LEFT JOIN Observer AS o ON f.fname = o.fname WHERE o.fname iS NULL; <br></p>
             <input type='submit' name="canned_query_5" value="Submit Query 5"><br>
         </form>
 
